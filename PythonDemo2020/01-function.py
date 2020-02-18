d0cd4d5f2bbc5e87fffa457cd2cb97712b149835
henggao geng <genghenggao@outlook.com>
2020-02-18 21:54:13 +0800

    henggao

---
+++ PythonDemo2020/01-function.py
@@ -0,0 +1,18 @@
+'''
+@Description: 
+@Version: 1.0
+@Autor: Henggao
+@Date: 2020-02-18 21:24:58
+@LastEditors: Henggao
+@LastEditTime: 2020-02-18 21:28:02
+'''
+def calculate(x,y):
+    '''
+    @param {type} 
+    @return: 
+    '''
+    s = x * y
+    return s
+
+n = calculate(2,3)
+print(n)
